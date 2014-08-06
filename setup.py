944e674f7a5c1ed1bb199e2644d73d7b129e4366
Travis Painter <travis.painter@gmail.com>
2014-08-05 21:39:56 -0500

    Add setup.py file to use with cx_freeze to create self-contained executible.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os", "zope.interface"], 
+                    "excludes": ["tkinter"],
+                    "includes": ["pkg_resources"],
+                    "namespace_packages": ["zope"],
+                    "silent": True}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application). This isn't a gui application, so don't change.
+base = None
+
+setup(  name = "df_everywhere",
+        version = "0.1",
+        description = "df_everywhere",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("tile_server.py", base=base)])
