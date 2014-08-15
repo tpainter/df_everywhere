8ba00af6a2c080448009a1349c73a6be664b5e4f
Travis Painter <travis.painter@gmail.com>
2014-08-15 14:18:06 -0500

    Include dfeverywhere.conf and tileset directory in build.

--- df_everywhere/setup.py
+++ df_everywhere/setup.py
@@ -4,6 +4,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os", "zope.interface"], 
                     "excludes": ["tkinter"],
+                    "include_files": ['dfeverywhere.conf', 'tilesets\\'],
                     "includes": ["pkg_resources"],
                     "namespace_packages": ["zope"],
                     "silent": True}
