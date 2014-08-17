31952ee87fdae5a179c7dc51518a15056ddbdfc2
Travis Painter <travis.painter@gmail.com>
2014-08-17 07:59:42 -0500

    Added welcome message.

--- df_everywhere/df_everywhere.py
+++ df_everywhere/df_everywhere.py
@@ -1,16 +1,16 @@
 # DF Everywhere
 # Copyright (C) 2014  Travis Painter
-
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     from twisted.internet import reactor
     from twisted.internet.defer import inlineCallbacks    
     
-    from util import wamp_local, utils, tileset, sendInput    
+    from util import wamp_local, utils, tileset, sendInput, messages
     
     #Change this to True for enhanced debugging    
     edebug = False
@@ -37,6 +37,8 @@ if __name__ == "__main__":
         #Uncomment the two lines below to get more detailed errors
         from twisted.internet.defer import setDebugging
         setDebugging(True)
+    
+    messages.welcome()
     
     #If 'localTest' file is present, use separate configuration
     import os.path
