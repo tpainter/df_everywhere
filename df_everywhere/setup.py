b138e73aa5e4885d6c1d4116de27ee1c42983ac2
Travis Painter <travis.painter@gmail.com>
2014-08-18 06:55:34 -0500

    Version 0.1.2

--- df_everywhere/setup.py
+++ df_everywhere/setup.py
@@ -14,7 +14,7 @@ build_exe_options = {"packages": ["os", "zope.interface"],
 base = None
 
 setup(  name = "df_everywhere",
-        version = "0.1.1",
+        version = "0.1.2",
         description = "df_everywhere",
         options = {"build_exe": build_exe_options},
         executables = [Executable("df_everywhere.py", base=base)])
