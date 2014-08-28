8fffe4907a21d31e7b77a9ac8ed9234c4e3e3bb7
Travis Painter <travis.painter@gmail.com>
2014-08-28 13:07:59 -0500

    More timing tests.

--- df_everywhere/test/imageCompareTest.py
+++ df_everywhere/test/imageCompareTest.py
@@ -10,20 +10,25 @@ import mmh3
 
 a = numpy.random.rand(32,32,3) * 255
 a_img = Image.fromarray(a.astype('uint8')).convert('RGB')
+a_array = numpy.array(a_img)
 
 b = numpy.random.rand(32,32,3) * 255
 b_img = Image.fromarray(b.astype('uint8')).convert('RGB')
+b_array = numpy.array(b_img)
 
+c = numpy.random.rand(1000,1000,3) * 255
+c_img = Image.fromarray(c.astype('uint8')).convert('RGB')
+c_array = numpy.array(c_img)
 
 setup = '''
-from __main__ import a_img, b_img
+from __main__ import a_img, b_img, c_img
 def equal(im1, im2):
     return hash(im1.tostring()) == hash(im2.tostring()) 
 '''
 
 setup1 = '''
 import mmh3
-from __main__ import a_img, b_img
+from __main__ import a_img, b_img, c_img
 def equal(im1, im2):
     return mmh3.hash(im1.tostring()) == mmh3.hash(im2.tostring()) 
 '''
@@ -33,14 +38,14 @@ try:
     import ImageChops
 except:
     from PIL import ImageChops
-from __main__ import a_img, b_img
+from __main__ import a_img, b_img, c_img
 def equal(im1, im2):
     return ImageChops.difference(im1, im2).getbbox() is None 
 '''
 
 setup3 = '''
 import numpy
-from __main__ import a_img, b_img
+from __main__ import a_img, b_img, c_img
 def equal(im1, im2):
     arr1 = numpy.array(im1)
     arr2 = numpy.array(im2)
@@ -66,18 +71,65 @@ def equal(im1, im2):
     return True 
 '''
 
+setup5 = '''
+try:
+    import Image
+except:
+    from PIL import Image
+from __main__ import a_array, b_array, c_array
+def equal(im1, im2):
+    return (im1 == im2).all() 
+'''
+
+setup6 = '''
+try:
+    import Image
+except:
+    from PIL import Image
+import numpy
+from __main__ import a_array, b_array, c_array
+def equal(im1, im2):
+    return numpy.array_equal(im1, im2) 
+'''
+
+setup7 = '''
+try:
+    import Image
+except:
+    from PIL import Image
+import numpy
+from __main__ import a_array, b_array, c_array
+import mmh3
+def equal(im1, im2):
+    return mmh3.hash(im1.tostring()) == mmh3.hash(im2.tostring()) 
+'''
+
 print("Different images...")
 print("Hash compare: \t\t%f" % min(timeit.Timer('equal(a_img, b_img)', setup).repeat(7, 1000)))
 print("mmh3 compare: \t\t%f" % min(timeit.Timer('equal(a_img, b_img)', setup1).repeat(7, 1000)))
 print("Difference compare: \t%f" % min(timeit.Timer('equal(a_img, b_img)', setup2).repeat(7, 1000)))
 print("Numpy compare: \t\t%f" % min(timeit.Timer('equal(a_img, b_img)', setup3).repeat(7, 1000)))
 print("Pixel compare: \t\t%f" % min(timeit.Timer('equal(a_img, b_img)', setup4).repeat(7, 1000)))
+print("Numpy array: \t\t%f" % min(timeit.Timer('equal(a_array, b_array)', setup5).repeat(7, 1000)))
+print("Numpy array2: \t\t%f" % min(timeit.Timer('equal(a_array, b_array)', setup6).repeat(7, 1000)))
+print("Numpy array hash: \t%f" % min(timeit.Timer('equal(a_array, b_array)', setup7).repeat(7, 1000)))
 print("\nSame images...")
 print("Hash compare: \t\t%f" % min(timeit.Timer('equal(a_img, a_img)', setup).repeat(7, 1000)))
 print("mmh3 compare: \t\t%f" % min(timeit.Timer('equal(a_img, a_img)', setup1).repeat(7, 1000)))
 print("Difference compare: \t%f" % min(timeit.Timer('equal(a_img, a_img)', setup2).repeat(7, 1000)))
 print("Numpy compare: \t\t%f" % min(timeit.Timer('equal(a_img, a_img)', setup3).repeat(7, 1000)))
 print("Pixel compare: \t\t%f" % min(timeit.Timer('equal(a_img, a_img)', setup4).repeat(7, 1000)))
+print("Numpy array: \t\t%f" % min(timeit.Timer('equal(a_array, a_array)', setup5).repeat(7, 1000)))
+print("Numpy array2: \t\t%f" % min(timeit.Timer('equal(a_array, a_array)', setup6).repeat(7, 1000)))
+print("Numpy array hash: \t%f" % min(timeit.Timer('equal(a_array, a_array)', setup7).repeat(7, 1000)))
+print("\nLarge image...")
+print("Hash compare: \t\t%f" % min(timeit.Timer('equal(c_img, c_img)', setup).repeat(3, 100)))
+print("mmh3 compare: \t\t%f" % min(timeit.Timer('equal(c_img, c_img)', setup1).repeat(3, 100)))
+print("Difference compare: \t%f" % min(timeit.Timer('equal(c_img, c_img)', setup2).repeat(3, 100)))
+print("Numpy compare: \t\t%f" % min(timeit.Timer('equal(c_img, c_img)', setup3).repeat(3, 100)))
+print("Numpy array: \t\t%f" % min(timeit.Timer('equal(c_array, c_array)', setup5).repeat(3, 100)))
+print("Numpy array2: \t\t%f" % min(timeit.Timer('equal(c_array, c_array)', setup6).repeat(3, 100)))
+print("Numpy array hash: \t%f" % min(timeit.Timer('equal(c_array, c_array)', setup7).repeat(3, 100)))
 
 '''RESULTS
 
